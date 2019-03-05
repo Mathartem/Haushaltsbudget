ab4f361df6d18c384ced78e0a2567ce625ac0a0f
artem.yudin <artem.yudin@NBAY.viadico.local>
2019-03-05 23:37:53 +0100

    changed Types of Fields

--- src/org/myproject/budget/Bill.java
+++ src/org/myproject/budget/Bill.java
@@ -1,37 +1,20 @@
 package org.myproject.budget;
 	
 	import java.util.Date;
+	
+	import java.math.BigDecimal;
+	
+	import java.util.GregorianCalendar;
 
 	public class Bill {
-
-	    double amount;
+		
+	    BigDecimal amount;
+	   
+	    //Enum Type {Home/Rent, Food, Ensurance/Medical, Entertainment, Utilities, Transport};
 	    
-	    
-	    
-	    String type;
-	    
-	    
-	    
-	    Date date;
-	    
-	    
+	    GregorianCalendar date;
+	       
 	    String comment;
 	    
-	    void Controll_Of_Numeric_Value (double Value)
-	    {
-
-	            if (Value <= 0){System.out.println("Only positive amounts can be entered");}
-	            
-	            else if (Value-(int)Value != 0)  
-	                 {
-	            	  String Value_as_String = Double.toString(Value);
-	                  Value_as_String = Value_as_String.substring(Value_as_String.indexOf(".") + 1);
-	                  int Number_Of_Decimal_Places = Value_as_String.length();
-
-	                  if (Number_Of_Decimal_Places > 2) 
-	                     {System.out.println("An amount can have at the most two decimal places");}
-	                 }  
-
-	     }
 
 	}
--- src/org/myproject/budget/Income.java
+++ src/org/myproject/budget/Income.java
@@ -2,35 +2,19 @@ package org.myproject.budget;
 
 import java.util.Date;
 
-public class Income {
+import java.math.BigDecimal;
 
-    double amount;
-    
+import java.util.GregorianCalendar;
+
+public class Income {
+	
+    BigDecimal amount;
    
+    //Enum Type {Salary, Bonus, Leasing, Stock Exchange, Dividents on Stock};
     
-    String type;
-    
-   
-    
-    Date date;
-   
+    GregorianCalendar date;
+       
     String comment;
     
-    void Controll_Of_Numeric_Value (double Value)
-    {
-
-            if (Value <= 0){System.out.println("Only positive amounts can be entered");}
-            
-            else if (Value-(int)Value != 0)  
-                 {
-            	  String Value_as_String = Double.toString(Value);
-                  Value_as_String = Value_as_String.substring(Value_as_String.indexOf(".") + 1);
-                  int Number_Of_Decimal_Places = Value_as_String.length();
-
-                  if (Number_Of_Decimal_Places > 2) 
-                     {System.out.println("An amount can have at the most two decimal places");}
-                 }  
-
-     }
 
 }
